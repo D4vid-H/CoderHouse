c418e184b750a6a573f8fea74da158886bb10b93
David Hillton <davidhillton@gmail.com>
2021-12-09 07:09:05 -0300

    index con boostrap

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 ScrollReveal().reveal('.contenedorBanner' , {delay: 500, reset: true});
 ScrollReveal().reveal('.elementoLi' , {delay: 500, reset: true});
+
+var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
